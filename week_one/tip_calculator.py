1c51a93c888fd867da744fb8851598f2070dfde8
tylerLoh <tyleryin811@gmail.com>
2019-12-15 21:42:31 +0800

    re- alignment and final commit for week one

--- week_one/tip_calculator.py
+++ week_one/tip_calculator.py
@@ -11,10 +11,16 @@ print("Welcome to Tip Calculator")
 print("=========================")
 print()
 
+# To keep asking/capture user input until is a valid number
 while True:
+
     bill_amount = input("Enter your bill amount: ")
+
+    # replce dolalr sign
     bill_amount = bill_amount.replace("$", '')
     print()
+
+    # try catch is a valid number
     try:
         bill_amount = float(bill_amount)
     except ValueError:
@@ -23,9 +29,9 @@ while True:
     else:
         break
 
+# tip level dict in percentage
 tip_level = [15, 18, 20]
 
 print("Bill tip recommendation:")
 for level in tip_level:
-    print(f"    {level}% = {level/100 * bill_amount:.1f}")
-
+    print(f"    {level}% = {level / 100 * bill_amount:.1f}")
